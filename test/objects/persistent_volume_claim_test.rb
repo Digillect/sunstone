0f46f1fede891df24467b928556984b1dbdd9bba
Gregory Nickonov <gregoryn@actis.ru>
2019-02-24 19:04:47 +0300

    Add serialization test for PersistentVolumeClaim

--- test/objects/persistent_volume_claim_test.rb
+++ test/objects/persistent_volume_claim_test.rb
@@ -23,4 +23,29 @@ class PersistentVolumeClaimTest < Minitest::Test
 
     assert_equal '8Gi', @sut.to_hash[:spec][:resources][:request][:storage]
   end
+
+  def test_serialization
+    @sut.access_mode :ReadWriteOnce
+    @sut.storage_class_name :slow
+    @sut.storage_request '5Gi'
+
+    expected = {
+      apiVersion: 'core/v1',
+      kind: 'PersistentVolumeClaim',
+      metadata: {
+        name: 'test'
+      },
+      spec: {
+        accessModes: ['ReadWriteOnce'],
+        storageClassName: 'slow',
+        resources: {
+          request: {
+            storage: '5Gi'
+          }
+        }
+      }
+    }
+
+    assert_equal_hash expected, @sut.to_hash
+  end
 end
